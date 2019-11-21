9e2123973dd3ca73cd2446e869b7318b578e3450
yangzhang214024 <yangzhang214024@sohu-inc.com>
2019-11-21 20:34:57 +0800

    add code for timeout

--- TaskDispatch/master/timeout.py
+++ TaskDispatch/master/timeout.py
@@ -3,29 +3,123 @@
 # @Author  : ZhangYang
 # @Email   : ian.zhang.88@outlook.com
 
-class TimeoutEvent():
+from datetime import datetime, timedelta
+import json
+from .utility import create_new_sequence_node
+
+import traceback, sys
+
+class TimeoutEvent(): # could break into two class
+
+    def __init__(self, zk_client, path):
+        self.zk_client = zk_client
+        self.path = path
+
+        self.task_path =None
+        self.type=None
+        self.callback_func=None
+        self.parameters=None
+        self.time_out=None
+        self.interval=None
+        self.final_time=None
+
+    def init_time(self, final_time, interval = None, now = None):
+        self.final_time = final_time
+        if interval:
+            self.interval = interval
+            self.time_out = now + interval
+        else:
+            self.time_out = final_time
+
+    def check_time_out(self,now):
+        if not self.interval:
+            return now > self.final_time
+
+        if now > self.time_out:
+            self.time_out += self.interval
+            return True
+
+    def check_final(self,now):
+        return now > self.final_time
+
+    def sync(self):
+        data = {
+            "task_path": self.task_path,
+            "type": self.type,
+            "callback_func": self.callback_func,
+            "parameters": self.parameters,
+            "time_out":self.time_out.strftime('%Y-%m-%d %H:%M:%S'),
+            "interval": self.interval.total_seconds() if self.interval else None,
+            "final_time":self.final_time.strftime('%Y-%m-%d %H:%M:%S'),
+        }
+
+        data_str = json.dumps(data).encode('utf-8')
+
+        self.zk_client.set(self.path, data_str)
+
+    def load(self):
+        data_str = self.zk_client.get(self.path)[0]
+        data = json.loads(data_str)
+
+        self.task_path = data['task_path']
+        self.type = data['type']
+        self.callback_func = data['callback_func']
+        self.parameters = data['parameters']
+        self.time_out = datetime.strptime(data['time_out'], '%Y-%m-%d %H:%M:%S')
+        self.interval = timedelta(seconds=data['interval']) if data['interval'] else None
+        self.final_time = datetime.strptime(data['final_time'], '%Y-%m-%d %H:%M:%S')
+
+    def delete(self):
+        self.zk_client.delete(self.path)
+
+class TimeoutCallbackRegister():
     def __init__(self):
         pass
 
 class TimeoutManager():
-    def __init__(self, zk_client):
+    def __init__(self, zk_client, base_path):
         self.zk_client = zk_client
+        self.base_path = base_path
+        self.zk_client.ensure_path(self.base_path)
+
         self._timeout_event_list = []
-        pass
+
+        self._load_from_server()
 
     def check(self):
         pass
 
     def _load_from_server(self):
-        pass
+        all_timeout = self.zk_client.get_children(self.base_path)
+        # print('----------- all_timeout', all_timeout)
+        # traceback.print_stack(file=sys.stdout)
+        for path in all_timeout:
+            toe = TimeoutEvent(self.zk_client, self.base_path+'/'+path)
+            toe.load()
 
-    def add(self):
-        pass
+            self._timeout_event_list.append(toe)
+
+    def add(self, type, task_path, callback_func, parameters, final_time, interval = None):
+        timeout_event_path = create_new_sequence_node(self.zk_client,self.base_path,'timeout')
+        toe = TimeoutEvent(self.zk_client, timeout_event_path)
+
+        toe.type = type
+        toe.task_path = task_path
+        toe.callback_func = callback_func
+        toe.parameters = parameters
+        if interval:
+            toe.init_time(final_time, interval, datetime.now())
+        else:
+            toe.init_time(final_time)
+
+        toe.sync()
+
+        self._timeout_event_list.append(toe)
+
 
     def _delete(self, timeout_event):
-        pass
+        timeout_event.delete()
 
-    def _update(self, timeout_event):
-        pass
+
 
 
--- TaskDispatch/tests/test_timeout.py
+++ TaskDispatch/tests/test_timeout.py
@@ -10,25 +10,86 @@ from ..master.core import Core
 # from ..master.job import Job
 # from ..master.cluster import Cluster
 from ..master.consts import TaskStateCode, TimeoutType
-from ..master.timeout import TimeoutManager
+from ..master.timeout import TimeoutManager, TimeoutEvent
 import datetime
 import json
 import random
+from datetime import datetime, timedelta
 
-class TestTask(unittest.TestCase, ZkClientMixin):
+
+class TimeoutCallbackRegisterTest():
+    def __init__(self):
+        pass
+
+class TestTimeout(unittest.TestCase, ZkClientMixin):
 
     def setUp(self):
         ZkClientMixin.__init__(self)
 
         self.zk_client = self.get_client()
         self.zk_client.delete('/testTimeout',recursive=True)
+        self.zk_client.create('/testTimeout', ''.encode('utf-8'))
 
         #self.core = Core(self.zk_client, '/testTimeout/core')
         self.timeout_manager = TimeoutManager(self.zk_client, '/testTimeout/timeout')
         #register = TimeoutCallbackRegister(self.core)
-        register = TimeoutCallbackRegisterTest()
+        #register = TimeoutCallbackRegisterTest()
 
-        self.timeout_manager.set_callback_register(register)
+        # self.timeout_manager.set_callback_register(register)、
+
+    def test_timeout_event(self):
+        self.zk_client.create('/testTimeout/timeout_test1', ''.encode('utf-8'))
+        self.zk_client.create('/testTimeout/timeout_test2', ''.encode('utf-8'))
+
+        to1 = TimeoutEvent(self.zk_client, '/testTimeout/timeout_test1/')
+        now = datetime.strptime('2018-01-01 00:00:00','%Y-%m-%d %H:%M:%S')
+        final_time = datetime.strptime('2018-01-01 00:05:00','%Y-%m-%d %H:%M:%S')
+        interval = timedelta(seconds=30)
+
+        to1.init_time(final_time, interval, now)
+        to1.type = TimeoutType.Job
+        to1.task_path= 'test_task'
+        to1.callback_func='test'
+        to1.parameters={"key":"value"}
+
+        to1.sync()
+
+        to1_clone = TimeoutEvent(self.zk_client, '/testTimeout/timeout_test1/')
+        to1_clone.load()
+
+        self.assertEqual(to1.type, to1_clone.type)
+        self.assertEqual(to1.task_path, to1_clone.task_path)
+        self.assertEqual(to1.callback_func, to1_clone.callback_func)
+        self.assertEqual(to1.parameters, to1_clone.parameters)
+        self.assertEqual(to1.time_out, to1_clone.time_out)
+        self.assertEqual(to1.interval, to1_clone.interval)
+        self.assertEqual(to1.final_time, to1_clone.final_time)
+
+        now_tmp = now
+        timeout_count = 0
+        for _ in range(5*60+1):
+            now_tmp+=timedelta(seconds=1)
+
+            if to1.check_time_out(now_tmp):
+                timeout_count+=1
+
+        self.assertEqual(timeout_count, 10)
+        self.assertTrue(to1.check_final(now_tmp))
+
+        to2 = TimeoutEvent(self.zk_client, '/testTimeout/timeout_test2/')
+        final_time = datetime.strptime('2018-01-01 00:05:00', '%Y-%m-%d %H:%M:%S')
+
+        to2.init_time(final_time)
+        to2.type = TimeoutType.Task
+        to2.callback_func = 'test'
+        to2.parameters = {"key": "value"}
+
+        now = datetime.strptime('2018-01-01 00:00:00', '%Y-%m-%d %H:%M:%S')
+        self.assertFalse(to2.check_time_out(now))
+        self.assertFalse(to2.check_final(now))
+        now = datetime.strptime('2018-01-01 00:05:01', '%Y-%m-%d %H:%M:%S')
+        self.assertTrue(to2.check_time_out(now))
+        self.assertTrue(to2.check_final(now))
 
     def test_add(self):
         self._add_time_out()
@@ -38,30 +99,38 @@ class TestTask(unittest.TestCase, ZkClientMixin):
         self.zk_client.create('testTimeout/task02', ''.encode('utf-8'))
         self.zk_client.create('testTimeout/task03', ''.encode('utf-8'))
 
+        final_time = datetime.strptime('2018-01-01 00:00:00', '%Y-%m-%d %H:%M:%S')
+        interval = timedelta(seconds=300)
+
         self.timeout_manager.add(TimeoutType.Task, 'testTimeout/task01', 'test_print', {'content': 'hello1'},
-                                 '2018-01-01 00:00:00', 300)
+                                 final_time, interval)
         self.timeout_manager.add(TimeoutType.Task, 'testTimeout/task02', 'test_print', {'content': 'hello2'},
-                                 '2018-01-01 00:00:00')
+                                 final_time)
         self.timeout_manager.add(TimeoutType.Task, 'testTimeout/task03', 'test_print', {'content': 'hello3'},
-                                 '2018-01-01 00:00:00')
+                                 final_time)
+
+        # test load
+        timeout_manager_tmp = TimeoutManager(self.zk_client, '/testTimeout/timeout')
+        self.assertEqual(len(timeout_manager_tmp._timeout_event_list),3)
+        for i in self.timeout_manager._timeout_event_list:
+            self.assertEqual(i.callback_func, 'test_print')
 
         # here only test task01
         found = False
         for i in self.timeout_manager._timeout_event_list:
-            if i.path != 'testTimeout/task01':
+            if '01' not in i.task_path:
                 continue
 
             found = True
-            timeout_event_path = i.base_path
-
-            data = self.zk_client.get('timeout_event_path')
+            data = self.zk_client.get(i.path)[0]
 
             dict_data = json.loads(data)
 
-            self.assertEqual('testTimeout/task01', dict_data['path'])
-            self.assertEqual('test_print', dict_data['test_print'])
-            self.assertEqual({'content': 'hello1'}, dict_data['param'])
-            self.assertEqual(300, dict_data['interval'])
-
+            self.assertEqual('test_print', dict_data['callback_func'])
+            self.assertEqual({'content': 'hello1'}, dict_data['parameters'])
+            self.assertEqual(interval, timedelta(seconds= dict_data['interval']))
 
         self.assertTrue(found)
+
+    def test_for_nothing(self):
+        self.assertTrue(True)
